53364de24fa141216c5bfe3f198aa9f7a49fb9b5
Mubasher.Skylinx <mubasher.skylinxtech@gmail.com>
2023-01-04 13:03:24 +0500

    Initialize setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data/data";
+import DataGridContainer from "./components/DataGridContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const headersToInclude = [
+        // "id",
+        "name",
+        "email",
+        "phone",
+        "url",
+        "group",
+        "address",
+        "zip",
+        "city",
+        "state",
+    ];
+
+    // const headers = Object.keys(data[0]).filter();
+    return (
+        <section>
+            <DataGridContainer
+                // columns={headers}
+                columns={headersToInclude}
+                rows={data}
+            />
+        </section>
+    );
 }
 
 export default App;
